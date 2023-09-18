b06b1d7c98623399e796791557561419722a4be1
DanielAraujoFaria <crosscross2.706@gmail.com>
2023-09-18 09:31:24 -0300

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Aula de Deploy - Netifly</h2>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
